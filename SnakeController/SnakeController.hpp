d8b9c0900686a9f88e1201f5e67772ce7630c08a
Michał Warzecha <m_warzecha@wp.pl>
2022-03-29 17:53:33 +0200

    Different  method for FoodInd

--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -30,6 +30,8 @@ public:
     Controller(Controller const& p_rhs) = delete;
     Controller& operator=(Controller const& p_rhs) = delete;
 
+    void receive(std::unique_ptr<Event<FoodInd>> e) override;
+
     void receive(std::unique_ptr<Event> e) override;
 
 private:
